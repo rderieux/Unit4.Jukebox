ee7e8723a34eae5e1f67b9efc889b136f889122e
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-15 17:16:51 -0600

    Add logging, 404, and error-handling middleware.  Add router file for users, playlists, & tracks

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+
+//Logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+//404 catchall middleware
+app.use((req, res, next) => {
+  next({ status: 404, message: "Endpoint doesn't exist." });
+});
+
+//Error-handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status ?? 500);
+  res.json(err.message ?? "Sorry: something went wrong.  Too bad, so sad.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port#: ${PORT}...`);
+});
