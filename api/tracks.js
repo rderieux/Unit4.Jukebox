71c8ea634ffd13b2bdfdb96caca8d599e9dab6d5
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-15 18:55:28 -0600

    Add router endpoints for tracks, get all, & get by id

--- api/tracks.js
+++ api/tracks.js
@@ -3,3 +3,32 @@ const router = express.Router();
 module.exports = router;
 
 const prisma = require("../prisma");
+
+router.get("/", async (req, res, next) => {
+  try {
+    const tracks = await prisma.track.findMany();
+    res.json(tracks);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const track = await prisma.track.findUniqueOrThrow({
+      where: { id: +id },
+    });
+
+    if (track) {
+      res.json(track);
+    } else {
+      next({
+        status: 404,
+        message: `Track with id#: ${id} does not exist.`,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
